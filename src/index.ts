93a218325b3e23f78684e33f1f706345a841ae4e
Matt <hey@mattt.space>
2024-10-29 10:47:47 -0700

    ci(Vec1): build out tests

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,9 @@ export type { Vector, Vector3Like } from './3D/index.ts';
 export { Vec2 } from './2D/index.ts';
 export type { Vector2Like } from './2D/index.ts';
 
+export { Vec1 } from './1D/index.ts';
+export type { Vector1Like } from './1D/index.ts';
+
 export { VecN } from './N/index.ts';
 export type { VectorNLike } from './N/index.ts';
 
---
+++ tests/Vec1/Vec1.test.ts
@@ -0,0 +1,170 @@
+import { describe, it } from 'jsr:@std/testing/bdd';
+import { expect } from 'jsr:@std/expect';
+
+import { Vec1, Vec2, Vec3, VecN } from '../../src/index.ts';
+import { Matrix } from '../../src/math/matrix.ts';
+
+describe('Vec1', () => {
+    it('get coords', () => {
+        const v = new Vec1(1);
+		expect(v.coord).toEqual(1);
+	});
+
+    it('set coords', () => {
+        const v = new Vec1(1);
+        v.coord = 2;
+        expect(v.coord).toEqual(2);
+    });
+
+    it('clone', () => {
+        const v = new Vec1(1);
+        const c = v.clone();
+        expect(c).toEqual(v);
+    });
+
+    it('copy', () => {
+        const v = new Vec1(1);
+        const u = new Vec1(2);
+        v.copy(u.coord);
+        expect(v).toEqual(u);
+    });
+
+    it('zero', () => {
+        const v = new Vec1(1);
+        v.zero();
+        expect(v.coord).toEqual(0);
+    });
+
+    it('unit', () => {
+        const v = new Vec1(0);
+        v.unit();
+        expect(v.coord).toEqual(1);
+    });
+
+    it('antiparallel', () => {
+        const v = new Vec1(3);
+
+        v.antiparallel();
+        expect(v.coord).toEqual(-3);
+    });
+
+    it('max', () => {
+        const v = new Vec1(1);
+        const u = new Vec1(2);
+
+        v.max(u.coord);
+        expect(v.coord).toEqual(2);
+    });
+
+    it('min', () => {
+        const v = new Vec1(1);
+        const u = new Vec1(2);
+
+        v.min(u.coord);
+        expect(v.coord).toEqual(1);
+    });
+
+    it('ceil', () => {
+        const v = new Vec1(1.02);
+
+        v.ceil()
+        expect(v.coord).toEqual(2);
+    });
+
+    it('floor', () => {
+        const v = new Vec1(1.02);
+
+        v.floor()
+        expect(v.coord).toEqual(1);
+    });
+
+    it('round (up)', () => {
+        const v = new Vec1(1.5);
+
+        v.round()
+        expect(v.coord).toEqual(2);
+    });
+
+    it('round (down)', () => {
+        const v = new Vec1(1.4);
+
+        v.round()
+        expect(v.coord).toEqual(1);
+    });
+
+    it('clamp', () => {
+        const v = new Vec1(1);
+
+        v.clamp(2, 3)
+        expect(v.coord).toEqual(2);
+    });
+
+    it('add', () => {
+        const v = new Vec1(1);
+        const u = new Vec1(2);
+
+        v.add(u.coord);
+        expect(v.coord).toEqual(3);
+    });
+
+    it('sub', () => {
+        const v = new Vec1(1);
+        const u = new Vec1(2);
+
+        v.sub(u.coord);
+        expect(v.coord).toEqual(-1);
+    });
+
+    it('multiply', () => {
+        const v = new Vec1(2);
+
+        v.multiply(2);
+        expect(v.coord).toEqual(4);
+    });
+
+    it('divide', () => {
+        const v = new Vec1(2);
+
+        v.divide(2);
+        expect(v.coord).toEqual(1);
+    });
+
+    it('transform (to 1)', () => {
+        const v = new Vec1(1);
+        const m = new Matrix([[2]]);
+
+        const u = v.transform(m);
+        expect(u).toEqual(new Vec1(2));
+    });
+
+    it('transform (to 2)', () => {
+        const v = new Vec1(1);
+        const m = [[2], [2]];
+
+        const u = v.transform(m);
+        expect(u).toEqual(new Vec2(2, 2));
+    });
+
+    it('transform (to 3)', () => {
+        const v = new Vec1(1);
+        const m = [[2], [2], [2]];
+
+        const u = v.transform(m);
+        expect(u).toEqual(new Vec3(2, 2, 2));
+    });
+
+    it('transform (to N)', () => {
+        const v = new Vec1(1);
+        const m = [[2], [2], [2], [2]];
+
+        const u = v.transform(m);
+        expect(u).toEqual(new VecN([2, 2, 2, 2]));
+    });
+
+    it('extend', () => {
+        const v = new Vec1(1);
+
+        const u = v.extend();
+        expect(u).toEqual(new Vec2(1, 0));
+    });
+});
